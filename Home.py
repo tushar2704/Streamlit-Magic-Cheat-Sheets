e03e066e5e250379e71648b669fff405997a2d94
Tushar Aggarwal <Tushar.27041994@gmail.com>
2023-12-24 19:42:15 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,8 @@
 ##© 2023 Tushar Aggarwal. All rights reserved.
 ##Streamlit Magic Cheat Sheets
 #######################################################################################################
-# Full length is above 10000 lines and is avalibale on requests only. Follow me, I will realising that in my upcoming book too!
+#Full length is above 10000 lines and is avalibale when you buy me a beer (https://www.buymeacoffee.com/taggdata) 
+
 #######################################################################################################
 #Importind required libraries
 #######################################################################################################
@@ -10,22 +11,14 @@ from pathlib import Path
 import base64
 import sys
 from pathlib import Path
-script_dir = Path(__file__).resolve().parent
-project_root = script_dir.parent
-sys.path.append(str(project_root))
+
 import warnings
 warnings.filterwarnings("ignore")
 import os
 #######################################################################################################
 #Importing from SRC
 #######################################################################################################
-from src.Streamlit_Magic_Cheat_Sheets.components.header import *
-from src.Streamlit_Magic_Cheat_Sheets.components.body import *
-from src.Streamlit_Magic_Cheat_Sheets.components.navigation import *
-from src.Streamlit_Magic_Cheat_Sheets.components.siderbar import *
-from src.Streamlit_Magic_Cheat_Sheets.components.metrics import *
-from src.Streamlit_Magic_Cheat_Sheets.components.charts import *
-from src.Streamlit_Magic_Cheat_Sheets.components.test import *
+
 #######################################################################################################
 #Header of Streamlit Magic Cheat Sheets by github.com/tushar2704
 #######################################################################################################
